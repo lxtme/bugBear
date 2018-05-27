af7e947ced95e1c342f113cc886f420d1d213873
Jason <changyuanjian@gmail.com>
2018-05-27 11:34:29 +0800

    add less

--- config-overrides.js
+++ config-overrides.js
@@ -1,10 +1,15 @@
 const { injectBabelPlugin } = require('react-app-rewired');
 const rewireMobX = require('react-app-rewire-mobx');
+const rewireLess = require('react-app-rewire-less');
 
 module.exports = function override(config, env) {
-	config = injectBabelPlugin(['import', { libraryName: 'antd', libraryDirectory: 'es', style: 'css' }], config);
+    config = injectBabelPlugin(['import', { libraryName: 'antd', libraryDirectory: 'es', style: 'css' }], config);
 
     // use the MobX rewire
     config = rewireMobX(config,env);
-	return config;
+
+    // use the less rewire
+    config = rewireLess(config, env);
+    return config;
 };
+
