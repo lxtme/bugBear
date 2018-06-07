9bc41a19487322c931a8bf34d5b92c0e6de02853
lxtme <1115927853@qq.com>
2018-06-07 23:34:34 +0800

    style

--- src/page/Details/Overview/index.js
+++ src/page/Details/Overview/index.js
@@ -1,26 +1,26 @@
 import React, {Component} from 'react';
-import {Icon} from 'antd';
+import {Icon, Row, Col} from 'antd';
 import {BarChart, Bar, PieChart, Pie, Cell} from 'recharts';
 
-const data24 = [];
+const data24Hours = [];
 for (let i = 0; i < 24; i++) {
-    data24.push(
+    data24Hours.push(
         {
             name: i + ':00',
             uv: Math.random() * 100
         }
     )
 }
-const data30 = [];
+const data30Days = [];
 for (let i = 0; i < 30; i++) {
-    data30.push(
+    data30Days.push(
         {
             name: i + ':00',
             uv: Math.random() * 100
         }
     )
 }
-const percent1 = [
+const browserPercents = [
     {
         name: 'chrome',
         value: 345
@@ -39,19 +39,19 @@ const percent1 = [
     },
 ];
 const percentColor = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042'];
-const test = [];
+const browserList = [];
 
 function browserSum() {
     let sum = 0;
-    for (let i = 0; i < percent1.length; i++) {
-        sum = sum + percent1[i].value;
+    for (let i = 0; i < browserPercents.length; i++) {
+        sum = sum + browserPercents[i].value;
     }
     console.log(sum);
     return sum;
 }
 
-percent1.map((entry, index) => {
-        test.push(
+browserPercents.map((entry, index) => {
+        browserList.push(
             <li key={index}>
                 <span className="li-before" style={{backgroundColor: percentColor[index % percentColor.length]}}> </span>
                 <span className="brower">{entry.name}</span>
@@ -59,117 +59,128 @@ percent1.map((entry, index) => {
                 <span>{Math.round(entry.value / browserSum() * 100) + '%'}</span>
             </li>
         )
-
     }
 );
-
 
 class Overview extends Component {
     render() {
         return (
             <div className="details-overview">
-                <div className="first-box">
-                    <div className="row1">
-                        <span className="row1-l">第一次出现</span>
-                        <span><Icon type="info-circle-o"/></span>
-                    </div>
-                    <span className="appear-time">2018-06-06 10:00</span>
-                    <div className="appear-number">
-                        事件数：<span>12%</span>&nbsp;&nbsp;
-                        用户数：<span>11%</span>
-                    </div>
-                    <div className="box-bottom">
-                        日均时间数：<span>12343</span>
-                    </div>
-                </div>
-                <div className="first-box">
-                    <div className="row1">
-                        <span className="row1-l">过去24小时</span>
-                        <span><Icon type="info-circle-o"/></span>
-                    </div>
-                    <span className="appear-time">6560</span>
-                    <div className="appear-number">
-                        <BarChart width={350} height={40} data={data24}>
-                            <Bar dataKey="uv" fill='#8884d8'/>
-                        </BarChart>
-                    </div>
-                    <div className="box-bottom">
-                        转换率：<span>60%</span>
-                    </div>
-                </div>
-                <div className="first-box">
-                    <div className="row1">
-                        <span className="row1-l">过去30天</span>
-                        <span><Icon type="info-circle-o"/></span>
-                    </div>
-                    <span className="appear-time">6560</span>
-                    <div className="appear-number">
-                        <BarChart width={350} height={40} data={data30}>
-                            <Bar dataKey="uv" fill='#8884d8'/>
-                        </BarChart>
-                    </div>
-                    <div className="box-bottom">
-                        转换率：<span>60%</span>
-                    </div>
-                </div>
-                <div className="percent-box">
-                    <div className="title">
-                        <span className="title-l">浏览器类别占比</span>
-                        <span><Icon type="info-circle-o"/></span>
-                    </div>
-                    <div className="percent-chart">
-                        <PieChart height={150} width={150} marginRight={20}>
-                            <Pie data={percent1} dataKey="value" fill='#8884d8'
-                                 cx={75} cy={75} innerRadius={50}
-                                 outerRadius={70}
-                            >
-                                {
-                                    percent1.map((entry, index) => {
-                                            return (
-                                                <Cell key={index} fill={percentColor[index % percentColor.length]}/>
+                <Row style={{width:'100%'}}>
+                    <Col span={7}>
+                        <div className="first-box">
+                            <div className="row1">
+                                <span className="row1-l">第一次出现</span>
+                                <span><Icon type="info-circle-o"/></span>
+                            </div>
+                            <span className="appear-time">2018-06-06 10:00</span>
+                            <div className="appear-number">
+                                事件数：<span>12%</span>&nbsp;&nbsp;
+                                用户数：<span>11%</span>
+                            </div>
+                            <div className="box-bottom">
+                                日均时间数：<span>12343</span>
+                            </div>
+                        </div>
+                    </Col>
+                    <Col span={7} offset={1}>
+                        <div className="first-box">
+                            <div className="row1">
+                                <span className="row1-l">过去24小时</span>
+                                <span><Icon type="info-circle-o"/></span>
+                            </div>
+                            <span className="appear-time">6560</span>
+                            <div className="appear-number">
+                                <BarChart width={350} height={40} data={data24Hours}>
+                                    <Bar dataKey="uv" fill='#8884d8'/>
+                                </BarChart>
+                            </div>
+                            <div className="box-bottom">
+                                转换率：<span>60%</span>
+                            </div>
+                        </div>
+                    </Col>
+                    <Col span={7} offset={1}>
+                        <div className="first-box">
+                            <div className="row1">
+                                <span className="row1-l">过去30天</span>
+                                <span><Icon type="info-circle-o"/></span>
+                            </div>
+                            <span className="appear-time">6560</span>
+                            <div className="appear-number">
+                                <BarChart width={350} height={40} data={data30Days}>
+                                    <Bar dataKey="uv" fill='#8884d8'/>
+                                </BarChart>
+                            </div>
+                            <div className="box-bottom">
+                                转换率：<span>60%</span>
+                            </div>
+                        </div>
+                    </Col>
+                </Row>
+                <Row style={{width:'100%'}}>
+                    <Col span={7}>
+                        <div className="percent-box">
+                            <div className="title">
+                                <span className="title-l">浏览器类别占比</span>
+                                <span><Icon type="info-circle-o"/></span>
+                            </div>
+                            <div className="percent-chart">
+                                <PieChart height={150} width={150} marginRight={20}>
+                                    <Pie data={browserPercents} dataKey="value" fill='#8884d8'
+                                         cx={75} cy={75} innerRadius={50}
+                                         outerRadius={70}
+                                    >
+                                        {
+                                            browserPercents.map((entry, index) => {
+                                                    return (
+                                                        <Cell key={index} fill={percentColor[index % percentColor.length]}/>
+                                                    )
+                                                }
                                             )
                                         }
-                                    )
-                                }
-                            </Pie>
-                        </PieChart>
-                        <div className="percent-chart-text">
-                            <ul>
-                                {test}
-                            </ul>
-
+                                    </Pie>
+                                </PieChart>
+                                <div className="percent-chart-text">
+                                    <ul>
+                                        {browserList}
+                                    </ul>
+                                </div>
+                            </div>
                         </div>
-                    </div>
-                </div>
-                <div className="percent-box">
-                    <div className="title">
-                        <span className="title-l">操作系统类别占比</span>
-                        <span><Icon type="info-circle-o"/></span>
-                    </div>
-                    <div className="percent-chart">
-                        <PieChart height={150} width={150} marginRight={20}>
-                            <Pie data={percent1} dataKey="value" fill='#8884d8'
-                                 cx={75} cy={75} innerRadius={50}
-                                 outerRadius={70}
-                            >
-                                {
-                                    percent1.map((entry, index) => {
-                                            return (
-                                                <Cell key={index} fill={percentColor[index % percentColor.length]}/>
+                    </Col>
+                    <Col span={7} offset={1}>
+                        <div className="percent-box">
+                            <div className="title">
+                                <span className="title-l">操作系统类别占比</span>
+                                <span><Icon type="info-circle-o"/></span>
+                            </div>
+                            <div className="percent-chart">
+                                <PieChart height={150} width={150} marginRight={20}>
+                                    <Pie data={browserPercents} dataKey="value" fill='#8884d8'
+                                         cx={75} cy={75} innerRadius={50}
+                                         outerRadius={70}
+                                    >
+                                        {
+                                            browserPercents.map((entry, index) => {
+                                                    return (
+                                                        <Cell key={index} fill={percentColor[index % percentColor.length]}/>
+                                                    )
+                                                }
                                             )
                                         }
-                                    )
-                                }
-                            </Pie>
-                        </PieChart>
-                        <div className="percent-chart-text">
-                            <ul>
-                                {test}
-                            </ul>
+                                    </Pie>
+                                </PieChart>
+                                <div className="percent-chart-text">
+                                    <ul>
+                                        {browserList}
+                                    </ul>
 
+                                </div>
+                            </div>
                         </div>
-                    </div>
-                </div>
+                    </Col>
+                </Row>
             </div>
         )
     }
